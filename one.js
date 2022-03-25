9f75cd33bb0bc37d3757ef2b645f60b5f5c330b4
ShehryarAhmed <princeshehryar6@gmail.com>
2022-03-25 17:12:46 +0500

    Commit Before Notes

--- one.js
+++ one.js
@@ -1,6 +1,19 @@
-let a=['a','x','s','b','f'];
-// a = String(a);
-// console.log(`M ${a.replace(/a/g,"x")}`)
-a.sort()
-console.log(a)
-// console.log(a)
+class Family {
+    constructor(Name, Age, Role) {
+        this.name = Name;
+        this.age = Age;
+        this.role = Role;
+    }
+    CallName(){
+        return this.name
+    }
+}
+P1 = new Family("Ahmed", "16", "Youngest Son");
+console.log(P1.CallName())
+console.log(Family.CallName)
+// class Employee {
+//   static add(a, b) {
+//     return a + b;
+//   }
+// }
+// console.log(Employee.add(4,5)))
